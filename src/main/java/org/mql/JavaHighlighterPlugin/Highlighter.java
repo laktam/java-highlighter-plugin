e09ca3e1a99daf4e830d307dd8fbd2e5dd0dbba2
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-25 19:05:56 +0100

    remove the Tokenezer and Highligher interfaces to use them from the jar of the main project

--- src/main/java/org/mql/JavaHighlighterPlugin/Highlighter.java
+++
@@ -1,7 +0,0 @@
-package org.mql.JavaHighlighterPlugin;
-
-import javax.swing.text.StyledDocument;
-
-public interface Highlighter {
-	public void highlight();
-}
--- src/main/java/org/mql/JavaHighlighterPlugin/Tokenizer.java
+++
@@ -1,8 +0,0 @@
-package org.mql.JavaHighlighterPlugin;
-
-import java.util.List;
-
-public interface Tokenizer {
-	public List<Token> tokenize(String code);
-	public String getTargetExtension();
-}
